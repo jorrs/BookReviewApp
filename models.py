67a57b9781ef25c7eb754154bafab1f7d0265033
jorrs <41180329+jorrs@users.noreply.github.com>
2024-05-31 07:49:17 +0200

    added new function

--- models.py
+++ models.py
@@ -51,8 +51,8 @@ class Review(db.Model):
 def create_book():
     data = request.get_json()
     new_book = Book(
-        title=data['title'], 
-        author=data['author'], 
+        title=data['title'],
+        author=data['author'],
         description=data.get('description')
     )
     db.session.add(new_book)
@@ -78,9 +78,28 @@ def create_review(book_id):
     return jsonify(new_review.to_json()), 201
 
 
+@app.route('/books/<int:book_id>/reviews/<int:review_id>', methods=['PUT'])
+def update_review(book_id, review_id):
+    review = Review.query.get(review_id)
+    if not review or review.book_id != book_id:
+        return jsonify({'message': 'Review not found or does not belong to this book'}), 404
+
+    data = request.get_json()
+    review.reviewer_name = data.get('reviewer_name', review.reviewer_name)
+    review.rating = data.get('rating', review.rating)
+    review.comment = data.get('comment', review.comment)
+    
+    db.session.commit()
+    return jsonify(review.to_json()), 200
+
+
 @app.route('/books', methods=['GET'])
 def get_books():
-    books = Book.query.all()
+    search = request.args.get('search')
+    if search:
+        books = Book.query.filter(Book.title.ilike(f'%{search}%') | Book.author.ilike(f'%{search}%')).all()
+    else:
+        books = Book.query.all()
     return jsonify([book.to_json() for book in books]), 200
 
 
