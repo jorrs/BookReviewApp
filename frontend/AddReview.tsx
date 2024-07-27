2045b7fafeba59c1476519f5807a6e341f70cf7c
jorrs <41180329+jorrs@users.noreply.github.com>
2024-07-27 23:52:26 +0200

    optimizing

--- frontend/AddReview.tsx
+++ frontend/AddReview.tsx
@@ -6,31 +6,33 @@ interface AddReviewProps {
 
 interface ReviewFormState {
   reviewerName: string;
-  reviewRating: string;
+  reviewRating: number; // Changed from string to number for direct use
   reviewComment: string;
 }
 
-const AddReview: React.FC<AddViewProps> = ({ onReviewSubmit }) => {
-  const [formState, setFormState] = useState<ReviewFormState>({ reviewerName: '', reviewRating: '', reviewComment: '' });
+const AddReview: React.FC<AddReviewProps> = ({ onReviewSubmit }) => {
+  // Initialized reviewRating as a number directly
+  const [formState, setFormState] = useState<ReviewFormState>({ reviewerName: '', reviewRating: 0, reviewComment: '' });
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setFormState({
-      ...formState,
-      [event.target.name]: event.target.value,
-    });
+    const { name, value } = event.target;
+    setFormState(prevState => ({
+      ...prevState,
+      [name]: name === 'reviewRating' ? Number(value) : value, // Direct conversion to number for reviewRating
+    }));
   };
 
   const handleFormSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    onReviewSubmit(formState.reviewerName, parseFloat(formState.reviewRating), formState.reviewComment);
-    // Reset form state after submitting
-    setFormState({ reviewerName: '', reviewRating: '', reviewComment: '' });
+    const { reviewerName, reviewRating, reviewComment } = formState;
+    onReviewSubmit(reviewerName, reviewRating, reviewComment);
+    setFormState({ reviewerName: '', reviewRating: 0, reviewComment: '' }); // Reset with initial value for reviewRating as number
   };
 
   return (
     <form onSubmit={handleFormSubmit}>
       <div>
-        <label htmlFor="reviewerName">Name:</label>
+        <label htmlFor="reviewerName">Name</label>
         <input
           type="text"
           id="reviewerName"
@@ -41,20 +43,20 @@ const AddReview: React.FC<AddViewProps> = ({ onReviewSubmit }) => {
         />
       </div>
       <div>
-        <label htmlFor="reviewRating">Rating:</label>
+        <label htmlFor="reviewRating">Rating</label>
         <input
           type="number"
           id="reviewRating"
           name="reviewRating"
           min="1"
           max="5"
-          value={formState.reviewRating}
+          value={formState.reviewRating.toString()} // Ensure the number is converted to a string for input value
           onChange={handleInputChange}
           required
         />
       </div>
       <div>
-        <label htmlFor="reviewComment">Comment:</label>
+        <label htmlFor="reviewComment">Comment</label>
         <textarea
           id="reviewComment"
           name="reviewComment"
